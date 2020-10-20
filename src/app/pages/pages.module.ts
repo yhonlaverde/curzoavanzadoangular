e00efb59936b005525db38a00a2cca523df07068
yhonlaverde <yhonlaverde0@gmail.com>
2020-10-20 12:22:32 -0500

    fin seccion cambiar colores y usar javascript en typescript

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 @NgModule({
@@ -16,13 +17,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     FormsModule,
--- src/app/pages/pages.rounting.ts
+++ src/app/pages/pages.rounting.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages.component';
 import {ProgressComponent} from './progress/progress.component'
 import {Grafica1Component} from './grafica1/grafica1.component'
 import {DashboardComponent} from './dashboard/dashboard.component'
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const routes: Routes = [
     {path: 'dashboard', component: PagesComponent,
@@ -11,6 +12,7 @@ const routes: Routes = [
       {path: '', component: DashboardComponent},
       {path: 'progress', component: ProgressComponent},
       {path: 'grafica1', component: Grafica1Component},
+      {path: 'account-settings', component: AccountSettingsComponent},
       // {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     ]},
 ];
